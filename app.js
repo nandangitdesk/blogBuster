bc797d7b1a4197252f6ab91ed9492abda373c60f
nandangitdesk <patilnandan859@gmail.com>
2024-07-23 16:02:25 +0530

    modified app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
@@ -7,34 +7,34 @@ const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
-const Blog = require("./models/blog")
+const Blog = require("./models/blog");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => console.log("mongodb connected"));
+  .then(() => console.log("mongodb connected"))
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(checkForAuthenticationCookie("token"))
-app.use(express.static(path.resolve("./public")))
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", async(req, res) => {
-  const allBlogs = await Blog.find({})
-  res.render("home" , {
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
     user: req.user,
     blogs: allBlogs
   });
 });
 
-app.use("/user", userRoute); //agar koi req /user se start hoti hai to useroute ka use karo
+app.use("/user", userRoute);
 app.use("/blog", blogRoute);
-
 
 app.listen(PORT, () => {
   console.log(`server started at PORT ${PORT}`);
